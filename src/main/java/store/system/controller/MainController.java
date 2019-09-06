1bda9c8212adf507e30b0873ffa4819ce9714867
Ekaterina <krasnovaea95@yandex.ru>
2019-09-06 14:02:20 -0400

    change project

---
+++ src/main/java/store/system/controller/MainController.java
@@ -0,0 +1,30 @@
+package store.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import store.system.model.User;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userJSP", new User());
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/check-user")
+    public ModelAndView checkUser(@ModelAttribute("userJSP") User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("secondPage");
+        modelAndView.addObject("userJSP", user);
+        return modelAndView;
+    }
+
+
+}

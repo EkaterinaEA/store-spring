27ac3a1fec1afb44b23a0a70e1e26289c4669797
Ekaterina <krasnovaea95@yandex.ru>
2019-09-10 17:19:05 -0400

    change project

---
+++ src/main/java/store/system/dao/OrderDAO.java
@@ -0,0 +1,77 @@
+package store.system.dao;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import store.system.model.Order;
+
+import java.util.List;
+
+public class OrderDAO {
+
+    private static final SessionFactory sessionFactory = HibernateFactory.getSessionFactory();
+
+    public static void saveOrUpdate(Order order){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        session.saveOrUpdate(order);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public static Order create(Order order){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        Integer id = (Integer) session.save(order);
+        session.getTransaction().commit();
+        session.close();
+        return OrderService.findById(id);
+    }
+
+    public static Order findOne(Integer id){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        Order ordersFromDB = (Order) session.find(Order.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return ordersFromDB;
+    }
+
+    public static Order findFirstByCartIdAndProductId(Integer cartId, Integer productId){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        Query q = session.createNativeQuery("SELECT * FROM orders WHERE cart_id=? AND product_id=?");
+        q.setParameter(1, cartId);
+        q.setParameter(2, productId);
+        Order orderFromDB = (Order) q.getResultList().get(0);
+        session.close();
+        return orderFromDB;
+    }
+
+    public static List<Order> findAll(){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        String sql = "SELECT * FROM orders";
+        List<Order> result = session.createNativeQuery(sql).getResultList();
+        return result;
+    }
+
+    public static List<Order> findAllOrdersByCartId(Integer cartId){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        Query q = session.createNativeQuery("SELECT * FROM orders WHERE cart_id=?");
+        q.setParameter(1, cartId);
+        List<Order> orderFromDB = q.getResultList();
+        session.close();
+        return orderFromDB;
+    }
+
+    public static void delete(Order order){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        session.delete(order);
+        session.getTransaction().commit();
+        session.close();
+    }
+}

5544e9f1b28f72ff66e098d390d71443f277e3ce
Ekaterina <krasnovaea95@yandex.ru>
2019-09-10 16:47:19 -0400

    change project

---
+++ src/main/java/store/system/model/Cart.java
@@ -0,0 +1,32 @@
+package store.system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Integer id;
+
+    @Column(name = "time")
+    private Long creationTime;
+
+    @Column(name = "closed")
+    private Boolean closed;
+
+    @ManyToOne(targetEntity = User.class)
+    @Column (name = "user")
+    private User user;
+
+    public Cart() { }
+
+}

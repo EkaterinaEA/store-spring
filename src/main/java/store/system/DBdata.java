9f7ef95f1b7b854d9dcc242bbf830c9dfe91cb90
Ekaterina <krasnovaea95@yandex.ru>
2019-09-16 11:32:25 -0400

    change project

--- src/main/java/store/system/DBdata.java
+++ src/main/java/store/system/DBdata.java
@@ -1,7 +1,10 @@
 package store.system;
 
+import store.system.dao.CartDAO;
+import store.system.model.Cart;
 import store.system.model.Item;
 import store.system.model.User;
+import store.system.service.CartService;
 import store.system.service.ItemService;
 import store.system.service.UserService;
 
@@ -11,10 +14,24 @@ public class DBdata {
 
     public static void main(String[] args) {
 
-        List<Item> items = ItemService.findAll();
-        for (int i=0; i< items.size(); i++) {
-            System.out.println(items.get(i).getName());
+        List<Cart> dataFromDB = CartService.findAll();
+
+        for (int i=0; i< dataFromDB.size(); i++) {
+            System.out.println(dataFromDB.get(i).getId());
+            System.out.println(dataFromDB.get(i).getClosed());
+            System.out.println(dataFromDB.get(i).getCreationTime());
+            System.out.println(dataFromDB.get(i).getUser());
+            System.out.println(dataFromDB.get(i).getUser().getName());
         }
+
+        for (int i=0; i<dataFromDB.size(); i++) {
+            CartService.delete(dataFromDB.get(i));
+        }
+
+        for (int i=0; i<dataFromDB.size(); i++) {
+            CartDAO.create(dataFromDB.get(i));
+        }
+
     }
 
 }
